9c08fe90c597bbdb2a867eaae46749b9a36c688d
TMDev24 <tm.dev12@outlook.com>
2024-09-21 20:38:49 +0100

    Add `MovieCarouselViewModelUnitTests` with empty tests

--- SimpleMessaging/SimpleMessagingTests/Feature/Feed/View Model/ChatFeedViewModelUnitTests.swift
+++ SimpleMessaging/SimpleMessagingTests/Feature/Feed/View Model/ChatFeedViewModelUnitTests.swift
@@ -5,8 +5,8 @@
 //  Created 21/09/2024.
 //
 
-import Foundation
 @testable import SimpleMessaging
+import XCTest
 
 protocol ChatFeedViewModelUnitTestsSpec {
     func test_init_defaultInitialisesWithExpectedTitle()
@@ -14,4 +14,24 @@ protocol ChatFeedViewModelUnitTestsSpec {
     func test_init_defaultInitialisesWithEmptyChatFeedModel()
     
     func test_loadFeed_setsViewModelStateToLoading()
+}
+
+typealias ChatFeedViewModelUnitTest = XCTestCase & ChatFeedViewModelUnitTestsSpec
+
+final class MovieCarouselViewModelUnitTests: ChatFeedViewModelUnitTest {
+    func test_init_defaultInitialisesWithExpectedTitle() {
+        
+    }
+    
+    func test_init_defaultInitialisesWithLoadedStateWithEmptyChatFeed() {
+        
+    }
+    
+    func test_init_defaultInitialisesWithEmptyChatFeedModel() {
+        
+    }
+    
+    func test_loadFeed_setsViewModelStateToLoading() {
+        
+    }
 }
